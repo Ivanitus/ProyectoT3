fcdf2814b4bdb6878cb8c7c08255a4218b8a63f4
Ivanitus <icamps555@gmail.com>
2019-05-19 13:25:53 +0200

    version 1.3.1
    cambios menores

--- d_tablas/TableableModel.java
+++ d_tablas/TableableModel.java
@@ -5,40 +5,38 @@ import java.util.Iterator;
 
 import javax.swing.table.DefaultTableModel;
 
-
 public class TableableModel extends DefaultTableModel {
 
-	
-	//Este iveride evita que las celdas de la tabla sean modificables
+	// Este override evita que las celdas de la tabla sean modificables
 	@Override
 	public boolean isCellEditable(int row, int column) {
 		// TODO Auto-generated method stub
 		return false;
 	}
-	
-	//Rellena la tabla con el ArrayList recibido, si le pasamos true tambien borrara los datos previos
+
+	// Rellena la tabla con el ArrayList recibido, si le pasamos true tambien
+	// borrara los datos previos
 	public void rellenarTabla(ArrayList<? extends Tableable> lista, boolean vaciar) {
-		
-		if(vaciar) {
+
+		if (vaciar) {
 			vaciarTabla();
 		}
-		
+
 		Iterator<? extends Tableable> it = lista.iterator();
-		while(it.hasNext()) {
+		while (it.hasNext()) {
 			Tableable t = it.next();
-			//Obetenemos la cabecera del modelo que implemento tableable y se lo establecemos como titulos de las columnas
+			// Obetenemos la cabecera del modelo que implemento tableable y se lo
+			// establecemos como titulos de las columnas
 			setColumnIdentifiers(t.getHeader());
-			//Obtenemos los datos del modelo y añadimos una nueva fila a la tabla
+			// Obtenemos los datos del modelo y añadimos una nueva fila a la tabla
 			addRow(t.getData());
-			
+
 		}
 	}
-	
+
 	public void vaciarTabla() {
 		setRowCount(0);
 		setColumnCount(0);
 	}
-	
-	
 
 }
